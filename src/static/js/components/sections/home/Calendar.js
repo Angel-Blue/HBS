1e68d13ab39ef6f0ec6dc09b8d39bc01e8758b39
Tara-Oliver <tara_m_oliver@yahoo.com>
2024-03-20 23:11:29 -0400

    initial template draft

---
+++ src/static/js/components/sections/home/Calendar.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import Content from "../events/Content";
+
+class Calendar extends Component {
+	render() {
+		return <Content />;
+	}
+}
+
+export default Calendar;
