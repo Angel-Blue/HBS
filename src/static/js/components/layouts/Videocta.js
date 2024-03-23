8e585c834fcfd76c2970f2ad514118ba4b770945
Tara-Oliver <tara_m_oliver@yahoo.com>
2024-03-23 15:30:11 -0400

    revised Videocta with HBS assets

--- src/static/js/components/layouts/Videocta.js
+++ src/static/js/components/layouts/Videocta.js
@@ -1,5 +1,6 @@
 import React from "react";
 import Popuphelper from "../../helper/popuphelper";
+import { Link } from "react-router-dom";
 
 class Videocta extends Popuphelper {
 	render() {
@@ -14,20 +15,32 @@ class Videocta extends Popuphelper {
 					<div className="row">
 						<div className="col-lg-8 custom-center">
 							<div className="advertisement-text-1 mb-30">
-								<h6 className="sub-head">
-									Understanding Katharine S. Day’s Identity
-								</h6>
+								<h6 className="sub-head">What We do</h6>
 								<h3 className="text-white heading">
-									Interpreting Sexuality in{" "}
-									<span className="text-color-secondary"> Early</span>{" "}
-									20th-century{" "}
-									<span className="text-color-secondary"> New England</span>
+									<span className="text-color-secondary">Preserve.</span>{" "}
+									Interpret.{" "}
+									<span className="text-color-secondary"> Inspire.</span>
 								</h3>
-								<p />
+								<p>
+									The Harriet Beecher Stowe Center’s mission is to encourage
+									social justice and literary activism by exploring the legacy
+									of Harriet Beecher Stowe.
+								</p>
+								<p>
+									Our vision is a world in which engagement leads to empathy,
+									empowerment, and change for good.
+								</p>
+								<Link
+									to="/about"
+									className="btn-solid with-line btn-big mt-20 mr-1">
+									<span>
+										Learn More <i className="fas fa-caret-right" />
+									</span>
+								</Link>
 							</div>
 						</div>
 						<div className="col-lg-4 align-self-center">
-							<div className="blob play-btn ">
+							{/* <div className="blob play-btn ">
 								<a
 									rel={"external"}
 									href={"https://www.youtube.com/embed/92U-vjAWUVM"}
@@ -35,7 +48,7 @@ class Videocta extends Popuphelper {
 									{" "}
 									<i className="fas fa-play" />
 								</a>
-							</div>
+							</div> */}
 						</div>
 					</div>
 				</div>
